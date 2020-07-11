d13a354bf38d9b63ad8a2db9506d0f391f92b8bc
lenovo <lenovo@LAPTOP-JO88T3FI>
2020-07-12 00:30:16 +0530

    Updated with mysql database

--- restful-web-services/src/main/java/com/trendy/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ restful-web-services/src/main/java/com/trendy/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,7 +2,9 @@ package com.trendy.rest.webservices.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages="com.trendy.rest.webservices.restfulwebservices")
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 
